e5bb8b3baead02d67bafbd9de4bf05aa230b2780
zarnigor <zarnigorasatova52@gmail.com>
2024-08-03 09:41:41 +0500

    feature

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import "./App.css";
 
 class App extends Component {
     state = {
-        firstName: "",
         email: "",
-        message: "",
-        nativeLang: "",
         agreement: false,
-        gender:"",
     };
 
     handleValue = (e) => {
@@ -31,22 +27,10 @@ class App extends Component {
     };
 
     render() {
-        const { firstName, email, message, nativeLang, agreement } = this.state;
+        const { email, agreement } = this.state;
         return (
             <div className="container">
                 <form>
-                    <div>
-                        <label htmlFor="firstName">First name</label>
-                        <input
-                            className="form-control"
-                            type="text"
-                            name="firstName"
-                            id="firstName"
-                            value={firstName}
-                            onChange={this.handleValue}
-                            onBlur={this.validateInput}
-                        />
-                    </div>
                     <div>
                         <label htmlFor="email">Email</label>
                         <input
@@ -56,36 +40,10 @@ class App extends Component {
                             id="email"
                             value={email}
                             onChange={this.handleValue}
+                            onBlur={this.validateInput}
                         />
                     </div>
-                    <div>
-                        <label htmlFor="message">Message</label>
-                        <textarea
-                            className="form-control"
-                            name="message"
-                            id="message"
-                            cols="30"
-                            rows="10"
-                            value={message}
-                            onChange={this.handleValue}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="nativeLang">Native Language</label>
-                        <select
-                            className="form-control"
-                            name="nativeLang"
-                            id="nativeLang"
-                            value={nativeLang}
-                            onChange={this.handleValue}
-                        >
-                            <option value="">Select a language</option>
-                            <option value="uz">Uzbek</option>
-                            <option value="ru">Russian</option>
-                            <option value="eng">English</option>
-                        </select>
-                    </div>
-                    <div>
+
                     <div className="form-check">
                         <input
                             className="form-check-input"
@@ -96,15 +54,11 @@ class App extends Component {
                             onChange={this.handleChecked}
                         />
                         <label className="form-check-label" htmlFor="agreement">
-                            Agreement
+                         Terms and Conditions
                         </label>
                     </div>
-                    </div>
                     <div>
-                        <label htmlFor="gender">Gender</label><br />
-                            <input type="radio" name="gender" id="gender" value="male" onChange={this.handleValue} />Male
-                            <input type="radio" name="gender" id="gender" value="female" onChange={this.handleValue} />Female
-
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </div>
                 </form>
             </div>
